be516fff45225058a9ac2d83dfcbc14be04a78fa
kzielin4 <k.zielinski@accenture.com>
2017-05-18 22:46:54 +0200

    test

--- POBR-REAL/src/Zielinski/Kamil/Model/Image.java
+++ POBR-REAL/src/Zielinski/Kamil/Model/Image.java
@@ -1,5 +1,9 @@
 package Zielinski.Kamil.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
@@ -12,13 +16,23 @@ public class Image
 		img = new Mat();
 	}
 
-	public void loadPicture(String path)
+	public int loadPicture(String path)
 	{
 		img = Imgcodecs.imread(path);
+		if (img.empty())
+		{
+			return 0;
+		}
+		return 1;
 	}
-	
+
 	public void testMethod()
 	{
 		System.out.println(img.depth());
+		System.out.println(img.rows());
+		System.out.println(img.get(100, 100));
+		List<Mat> lRgb = new ArrayList<Mat>(3);
+		Core.split(img, lRgb);
+		System.out.println(lRgb.get(0));
 	}
 }
